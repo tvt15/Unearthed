42ec83233fe39ad721f3ca02cae6be090e1bb330
TVT <tanishqtodkar@gmail.com>
2024-09-19 20:49:15 -0400

    setup 2

---
+++ server/data/gifts.js
@@ -0,0 +1,35 @@
+const giftData=
+[
+    {
+        "id": 1,
+        "name": "Disco Ball Candle",
+        "pricePoint": "$",
+        "audience": "Candle Lovers",
+        "image": "https://tinyurl.com/39xc9h6y",
+        "description": "A party in a candle",
+        "submittedBy": "Sasha",
+        "submittedOn": "2022-09-04T14:48:00"
+    },
+    {
+        "id": 2,
+        "name": "Gourmet Chocolate Box",
+        "pricePoint": "$$",
+        "audience": "Chocolate Enthusiasts",
+        "image": "https://tinyurl.com/2p8t4b4y",
+        "description": "A box of assorted gourmet chocolates",
+        "submittedBy": "Alex",
+        "submittedOn": "2022-10-12T09:30:00"
+    },
+    {
+        "id": 3,
+        "name": "Handmade Pottery Mug",
+        "pricePoint": "$$$",
+        "audience": "Art Lovers",
+        "image": "https://tinyurl.com/2p8t4b4z",
+        "description": "A beautifully crafted pottery mug",
+        "submittedBy": "Jordan",
+        "submittedOn": "2022-11-01T16:20:00"
+    }
+]
+
+export default giftData
