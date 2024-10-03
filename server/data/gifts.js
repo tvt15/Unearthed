17cc824b9a31d7cb1e1d18616aebe596e8fafdc4
TVT <tanishqtodkar@gmail.com>
2024-10-03 18:17:36 -0400

    updated gift data

--- server/data/gifts.js
+++ server/data/gifts.js
@@ -15,7 +15,7 @@ const giftData=
         "name": "Gourmet Chocolate Box",
         "pricePoint": "$$",
         "audience": "Chocolate Enthusiasts",
-        "image": "https://tinyurl.com/2p8t4b4y",
+        "image": "https://compartes.com/cdn/shop/products/Premium-Luxury-Chocolate-Dipped-Fruits-Gift-Box.jpg?v=1664146154",
         "description": "A box of assorted gourmet chocolates",
         "submittedBy": "Alex",
         "submittedOn": "2022-10-12T09:30:00"
@@ -25,7 +25,7 @@ const giftData=
         "name": "Handmade Pottery Mug",
         "pricePoint": "$$$",
         "audience": "Art Lovers",
-        "image": "https://tinyurl.com/2p8t4b4z",
+        "image": "https://stellancworks.com/cdn/shop/products/20220406_125939_1024x1024@2x.jpg?v=1652184454",
         "description": "A beautifully crafted pottery mug",
         "submittedBy": "Jordan",
         "submittedOn": "2022-11-01T16:20:00"
